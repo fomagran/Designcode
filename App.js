62067e3696d7f165a970f23d0c221da7296db29e
Young <fomagran6@naver.com>
2022-02-16 16:59:47 -0600

    [add] Styled Components in React Native

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar
+} from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import styled from 'styled-components';
+import Card from './components/Card';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( <Container >
+    <TitleBar >
+    <Avatar source={require("./assets/avatar.jpg")}/>
+    <Title > Welcom back, </Title> 
+    < Name > Fomagran </Name>
+    <StatusBar style = "auto" />
+    </TitleBar> 
+    <SubTitle>Continue Learning</SubTitle>
+    <Card />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const SubTitle = styled.Text`
+  color: #b8bece;
+  font-weight:600;
+  font-size:15px;
+  margin-left:20px;
+  margin-top:50px;
+  text-transform:uppercase;
+`;
+
+const Container = styled.View `
+  flex:1;
+  background-color:#f0f3f5;
+`;
+
+const Title = styled.Text `
+ font-size:16px;
+ color:#b8bece;
+ font-weight:500;
+`
+
+const Name = styled.Text `
+ font-size:20px;
+ color:#3c4560;
+ font-weight:bold;
+ `
+
+const TitleBar = styled.View `
+  width:100%;
+  margin-top:50px;
+  padding-left:80px;
+`
+
+const Avatar = styled.Image`
+  width:44px;
+  height:44px;
+  background:black;
+  border-radius:22px;
+  margin-left:20px;
+  position:absolute;
+  top:0;
+  left:0;
+`;
