ae6457af3e79e4d9dfb96c9de1ccd652f7235409
Young <fomagran6@naver.com>
2022-02-17 11:26:10 -0600

    -x버튼 위치 가운데로 조정 -x 버튼 누르면 화면 닫기 - 위 상단 메뉴화면 꾸미기

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { Animated } from "react-native";
+import { Animated, TouchableOpacity, Dimensions } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+//스크린 높이 알아내는 법
+const screenHeight = Dimensions.get("window").height;
 
 class Menu extends React.Component {
   state = {
-    top: new Animated.Value(900),
+    top: new Animated.Value(screenHeight),
   };
 
   componentDidMount() {
@@ -12,11 +16,35 @@ class Menu extends React.Component {
       toValue: 0,
     }).start();
   }
+  //TouchableOpacity 적용하면 아이콘 터치 가능
+  toggleMenu = () => {
+    Animated.spring(this.state.top, {
+      toValue: screenHeight,
+    }).start();
+  };
 
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
-        <Cover />
+        <Cover>
+          <Image source={require("../assets/background2.jpg")} />
+          <Title>Fomagran</Title>
+          <Subtitle>Designer at Design+Code</Subtitle>
+        </Cover>
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1,
+          }}
+        >
+          <CloseView>
+            <Ionicons name="ios-close" size={44} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
         <Content />
       </AnimatedContainer>
     );
@@ -24,6 +52,34 @@ class Menu extends React.Component {
 }
 
 export default Menu;
+
+const Image = styled.Image`
+    position:absolute
+    width:100%
+    height:100%
+`;
+
+const Title = styled.Text`
+color:white
+font-size:24px
+font-weight:600
+`;
+
+const Subtitle = styled.Text`
+    font-size:13px
+    color:rgba(255,255,255,0.5)
+    margin-top:8px
+`;
+
+const CloseView = styled.View`
+    width:44px
+    height:44px
+    border-radius:22px
+    background:white
+    justify-content:center
+    align-items:center
+    box-shadow:0 5px 10px rgba(0,0,0,0.15)
+`;
 
 const Container = styled.View`
     position:absolute;
@@ -38,9 +94,11 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container);
 const Cover = styled.View`
     height:142px
     background:black
+    justify-content:center
+    align-items:center
 `;
 
 const Content = styled.View`
-    height:900px
+    height:${screenHeight}
     background:#f0f3f5
 `;
