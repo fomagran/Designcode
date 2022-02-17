8bb5db1156edf1550037a894709ffb290fd2c3d5
Young <fomagran6@naver.com>
2022-02-17 09:21:37 -0600

    Props and Icons

--- App.js
+++ App.js
@@ -1,67 +1,107 @@
-import {
-  StatusBar
-} from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import styled from 'styled-components';
-import Card from './components/Card';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import Card from "./components/Card";
+import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 
+//code align alt+shift+f
+//image source ""
 export default function App() {
-  return ( <Container >
-    <TitleBar >
-    <Avatar source={require("./assets/avatar.jpg")}/>
-    <Title > Welcom back, </Title> 
-    < Name > Fomagran </Name>
-    <StatusBar style = "auto" />
-    </TitleBar> 
-    <SubTitle>Continue Learning</SubTitle>
-    <Card />
+  return (
+    <Container>
+      <SafeAreaView>
+        <ScrollView style={{ height: "100%" }}>
+          <TitleBar>
+            <Avatar source={require("./assets/avatar.jpg")} />
+            <Title> Welcome back, </Title>
+            <Name> Fomagran </Name>
+            <NotificationIcon
+              style={{ position: "absolute", right: 20, top: 5 }}
+            />
+            <StatusBar style="auto" />
+          </TitleBar>
+          <ScrollView
+            style={{
+              flexDirection: "row",
+              padding: 20,
+              paddingLeft: 12,
+              paddingTop: 30,
+            }}
+            horizontal={true}
+          >
+            <Logo
+              image={require("./assets/logo-framerx.png")}
+              text="Framer X"
+            />
+            <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+          </ScrollView>
+          <SubTitle>Continue Learning</SubTitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Card
+              title="Styled Components"
+              image={require("./assets/background2.jpg")}
+              caption="React Native"
+              logo={require("./assets/logo-react.png")}
+              subTitle="5 of 12 sections"
+            />
+            <Card
+              title="Styled Components 2"
+              image={require("./assets/background1.jpg")}
+              caption="React Native"
+              logo={require("./assets/logo-react.png")}
+              subTitle="5 of 12 sections"
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
 
 const SubTitle = styled.Text`
   color: #b8bece;
-  font-weight:600;
-  font-size:15px;
-  margin-left:20px;
-  margin-top:50px;
-  text-transform:uppercase;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 20px;
+  text-transform: uppercase;
 `;
 
-const Container = styled.View `
-  flex:1;
-  background-color:#f0f3f5;
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
 `;
 
-const Title = styled.Text `
- font-size:16px;
- color:#b8bece;
- font-weight:500;
-`
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
 
-const Name = styled.Text `
- font-size:20px;
- color:#3c4560;
- font-weight:bold;
- `
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
 
-const TitleBar = styled.View `
-  width:100%;
-  margin-top:50px;
-  padding-left:80px;
-`
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
 
 const Avatar = styled.Image`
-  width:44px;
-  height:44px;
-  background:black;
-  border-radius:22px;
-  margin-left:20px;
-  position:absolute;
-  top:0;
-  left:0;
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const Card = props => (
-    <Container>
-        <Cover> 
-            <Image source = {require("../assets/background2.jpg")}/>
-            <Title>Styled Components</Title>
-        </Cover>
-        <Content>
-            <Logo source = {require("../assets/logo-react.png")}/>
-            <Wrapper>
-            <Caption>React Native</Caption>
-            <Subtitle>5 of 12 sections</Subtitle>
-            </Wrapper>
-        </Content>
-        </Container>
-)
+const Card = (props) => (
+  <Container>
+    <Cover>
+      <Image source={props.image} />
+      <Title>{props.title}</Title>
+    </Cover>
+    <Content>
+      <Logo source={props.logo} />
+      <Wrapper>
+        <Caption>{props.caption}</Caption>
+        <Subtitle>{props.subtitle}</Subtitle>
+      </Wrapper>
+    </Content>
+  </Container>
+);
 
 export default Card;
 
@@ -24,18 +24,18 @@ const Content = styled.View`
     flex-direction:row
     align-items:center;
     height:80px;
-`
+`;
 
 const Logo = styled.Image`
     width:44px
     height:44px
-`
+`;
 
 const Caption = styled.Text`
     color:#3c4560
     font-size:20px;
     font-weight:600
-`
+`;
 
 const Subtitle = styled.Text`
 color:#b8bece
@@ -43,37 +43,35 @@ font-size:15px;
 font-weight:600
 text-transform:uppercase
 margin-top:4px
-`
+`;
 
 const Wrapper = styled.View`
-    margin-left:10px;
-`
-
-
+  margin-left: 10px;
+`;
 const Container = styled.View`
-    background:white;
-    width:315px;
-    height:280px;
-    border-radius:14px;
-    margin-left:20px;
-    margin-top:20px;
-    box-shadow:0 5px 15px rgba(0,0,0,0.15);
+  background: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  margin-left: 20px;
+  margin-top: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
 `;
 
 const Cover = styled.View`
-    width:100%;
-    height:200px;
-    border-top-left-radius:14px;
-    border-top-right-radius:14px;
-    overflow:hidden;
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
 `;
 
 const Image = styled.Image`
-    width:100%;
-    height:100%;
-    position:absolute;
-    top:0;
-    left:0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
 const Title = styled.Text`
