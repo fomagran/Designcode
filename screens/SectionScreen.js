622e4548e77bef74eb5097456be28619db977fd1
Young <fomagran6@naver.com>
2022-02-18 17:36:38 -0600

    MarkDown 글 변환해서 보여주고 꾸미기

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Ionicons } from "@expo/vector-icons";
-import { TouchableOpacity, StatusBar, Linking } from "react-native";
+import { TouchableOpacity, StatusBar, Linking, ScrollView } from "react-native";
 import { WebView } from "react-native-webview";
+import MarkdownView from "react-native-showdown";
 
 class SectionScreen extends React.Component {
   static navigationOptions = {
@@ -23,34 +24,35 @@ class SectionScreen extends React.Component {
     const section = navigation.getParam("section");
 
     return (
-      <Container>
-        <StatusBar hidden />
-        <Cover>
-          <Image source={{ uri: section.image.url }} />
-          <Wrapper>
-            <Logo source={{ url: section.logo.url }} />
-            <Subtitle>{section.subtitle}</Subtitle>
-          </Wrapper>
-          <Title>{section.title}</Title>
-          <Caption>{section.caption}</Caption>
-        </Cover>
-        <TouchableOpacity
-          onPress={() => {
-            this.props.navigation.goBack();
-          }}
-          style={{ position: "absolute", top: 20, right: 20 }}
-        >
-          <CloseView>
-            <Ionicons
-              name="ios-close"
-              size={24}
-              color="#4775f2"
-              style={{ marginTop: -2 }}
-            />
-          </CloseView>
-        </TouchableOpacity>
-        <Content>
-          <WebView
+      <ScrollView>
+        <Container>
+          <StatusBar hidden />
+          <Cover>
+            <Image source={{ uri: section.image.url }} />
+            <Wrapper>
+              <Logo source={{ url: section.logo.url }} />
+              <Subtitle>{section.subtitle}</Subtitle>
+            </Wrapper>
+            <Title>{section.title}</Title>
+            <Caption>{section.caption}</Caption>
+          </Cover>
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.goBack();
+            }}
+            style={{ position: "absolute", top: 20, right: 20 }}
+          >
+            <CloseView>
+              <Ionicons
+                name="ios-close"
+                size={24}
+                color="#4775f2"
+                style={{ marginTop: -2 }}
+              />
+            </CloseView>
+          </TouchableOpacity>
+          <Content>
+            {/* <WebView
             source={{ html: htmlContent + htmlStyle }}
             scalesPageToFit={false}
             scrollEnabled={false}
@@ -62,9 +64,16 @@ class SectionScreen extends React.Component {
                 Linking.openURL(event.url);
               }
             }}
-          />
-        </Content>
-      </Container>
+          /> */}
+            <MarkdownView
+              body={markDownSample}
+              pureCSS={htmlStyle}
+              scalesPageToFit={false}
+              scrollEnabled={false}
+            />
+          </Content>
+        </Container>
+      </ScrollView>
     );
   }
 }
@@ -78,11 +87,36 @@ const htmlContent = `
 `;
 
 const htmlStyle = `
-    <style>
     * {
         font-family:-apple-system, Roboto;
         margin:0
         padding:0
+        font-size:17px
+        font-weight:normal
+        color:#3c4560
+        line-height:24px
+    }
+
+    h2 {
+      font-size:20px
+      text-transform:uppercase
+      color:#b8bece
+      font-weight:600
+      margin-top:50px
+    }
+
+    p{
+      margin-top:20px
+    }
+
+    a {
+      color:#4775f2
+      font-weight:600
+      text-decoration:none
+    }
+
+    strong {
+      font-weight:700
     }
 
     img {
@@ -90,12 +124,11 @@ const htmlStyle = `
         border-radius:10px;
         margin-top:20px;
     }
-    </style>
 `;
 
 const Content = styled.View`
-  height: 100%;
-  padding: 20px;
+  height: 1000px
+  padding: 0px;
 `;
 
 const Wrapper = styled.View`
@@ -160,4 +193,111 @@ const Caption = styled.Text`
     bottom:20px
     left:20px
     width:300px
+`;
+
+const markDownSample = `
+---
+__Advertisement :)__
+
+- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image
+  resize in browser.
+- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly
+  i18n with plurals support and easy syntax.
+
+You will like those projects!
+
+---
+
+# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+
+## Horizontal Rules
+
+___
+
+---
+
+***
+
+
+## Typographic replacements
+
+Enable typographer option to see result.
+
+(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+test.. test... test..... test?..... test!....
+
+!!!!!! ???? ,,  -- ---
+
+"Smartypants, double quotes" and 'single quotes'
+
+
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
+
+## Blockquotes
+
+
+> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as 
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+
+## Code
+
+Inline
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
 `;
